89c89970dfa6ed07f2bdfe8c5cbd54a66da136b6
buhtelj <grcar.urban@gmail.com>
2014-01-04 18:50:01 +0100

    Ekoloska trgovina mobilna aplikacijaž

--- www/js/funkcije.js
+++ www/js/funkcije.js
@@ -25,12 +25,6 @@
 		});
 	}
 	
-	function izdelekPr(id_izd){
-		$("#izdelek").load("http://bplanet.si/web/ekoloska/strani/izdelek.php", {id_izdelka: id_izd}, function() { 
-			$('.loading').hide();
-			$('#content').hide();
-		});
-	}
 
 	function vKosarico(id){
 		$("#izdelek").load("http://bplanet.si/web/ekoloska/strani/cart/add.php", {id_izdelka: id}, function() { 
