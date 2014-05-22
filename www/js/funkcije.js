4e4f255372368e029db0a1643c702e8c8cadcfdc
buhtelj <grcar.urban@gmail.com>
2014-05-22 20:14:05 +0200

    href

--- www/js/funkcije.js
+++ www/js/funkcije.js
@@ -8,7 +8,7 @@
 	}
 
 	function zamenjajStran(id_kat) { 
-		window.location.assign("izbira.html");
+		window.location.href("izbira.html");
 		$("#page").load("http://bplanet.si/web/ekoloska/strani/izbira.php", { id_kategorije: id_kat}, function() { 
 			$('.loading').hide();
 		});
